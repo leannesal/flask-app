0c5ae6ba7394278a8d0a7a7bcedf3e06dec2c0c0
lsalame <lsalme@cisco.com>
2023-07-27 10:55:45 +0100

    creating create_app function inside __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,1 +1,24 @@
 
+from flask import Flask 
+
+def create_app():
+    #basedir = os.path.abspath(os.path.dirname(__file__))
+    app = Flask(__name__)
+
+    #secure session data: encrypt the session data and cookies related to our website
+    app.config['SECRET_KEY'] = 'Cisco123!'
+    
+    from .views import views
+    from .authentication import auth
+
+   # app.config['SQLALCHEMY_DATABASE_URI'] =\
+    #        'sqlite:///' + os.path.join(basedir, 'database.db')
+
+    app.config['SECRET_KEY'] = 'mysecret'
+
+    #register views
+    app.register_blueprint(views, url_prefix='/')
+
+    app.register_blueprint(auth, url_prefix='/')
+
+    return app
