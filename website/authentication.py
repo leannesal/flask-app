b41b1c9c0b7da8e1acaf90f26d00e08f3b1fdbfc
lsalame <lsalme@cisco.com>
2023-07-27 16:30:07 +0100

    making minor changes to authencation.py

--- website/authentication.py
+++ website/authentication.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth=Blueprint ('auth',__name__)
 
 @auth.route('/login')
 def login():
-    return 'login'
+    return render_template("login.html")
 
 @auth.route('/sign_up')
 def sign_up():
